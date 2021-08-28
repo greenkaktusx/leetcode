8fc301bc670f0c3f4bfcc4709a3731a58ef16388
Алексей Алексеев <a.alekseev@tektorg.ru>
2021-08-29 00:57:29 +0300

    Roman To Integer

---
+++ easy/roman_to_integer.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII,
+ * which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII. Instead, the number four is written as IV.
+ * Because the one is before the five we subtract it making four.
+ * The same principle applies to the number nine, which is written as IX.
+ * There are six instances where subtraction is used:
+ * - I can be placed before V (5) and X (10) to make 4 and 9.
+ * - X can be placed before L (50) and C (100) to make 40 and 90.
+ * - C can be placed before D (500) and M (1000) to make 400 and 900.
+ *
+ * Given a roman numeral, convert it to an integer.
+ */
+
+class Solution
+{
+    public $symbols = [
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000
+    ];
+
+    public $doubleSymbols = [
+        'I' => ['V', 'X'],
+        'X' => ['L', 'C'],
+        'C' => ['D', 'M']
+    ];
+
+    /**
+     * @param string $s
+     * @return int
+     */
+    public function romanToInt(string $s): int
+    {
+        if (isset($this->symbols[$s])) {
+            return $this->symbols[$s];
+        }
+
+        $number = 0;
+        $skipSymbol = false;
+
+        for ($i = 0; $i < strlen($s); $i++) {
+            if ($skipSymbol) {
+                $skipSymbol = false;
+                continue;
+            }
+
+            $currentSymbol = $s[$i];
+
+            if (isset($this->doubleSymbols[$currentSymbol]) && $i != (strlen($s) - 1)) {
+                $nextSymbol = $s[$i + 1];
+
+                if (in_array($nextSymbol, $this->doubleSymbols[$currentSymbol])) {
+                    $number += ($this->symbols[$nextSymbol] - $this->symbols[$currentSymbol]);
+                    $skipSymbol = true;
+                    continue;
+                }
+            }
+
+            $number += $this->symbols[$currentSymbol];
+        }
+
+        return $number;
+    }
+}
+
+$testCases = [
+    'III',
+    'IV',
+    'IX',
+    'LVIII',
+    'MCMXCIV',
+    'XII',
+    'XXVII'
+];
+
+$solution = new Solution();
+
+foreach ($testCases as $testCase) {
+    echo $solution->romanToInt($testCase) . "<br/>";
+}
